1002a2818199dda4ad91a8589e0f5f11653aabc3
Lucas Hungaro <lhungaro10@gmail.com>
2025-07-10 19:34:49 -0300

    feat: add create question, create room, get room questions

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,10 @@ import {
   type ZodTypeProvider,
 } from "fastify-type-provider-zod";
 import { env } from "./env.ts";
+import { createQuestionRoute } from "./http/routes/create-question.ts";
+import { createRoomRoute } from "./http/routes/create-room.ts";
 import { getRoomsRoute } from "./http/routes/get-roms.ts";
+import { getRoomQuestionsRoute } from "./http/routes/get-room-questions.ts";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>();
 app.register(fastifyCors, {
@@ -21,7 +24,9 @@ app.get("/health", () => {
 });
 
 app.register(getRoomsRoute);
-
+app.register(createRoomRoute);
+app.register(getRoomQuestionsRoute);
+app.register(createQuestionRoute);
 app.listen({ port: env.PORT }).then(() => {
   console.log(`HTTP server running on port ${env.PORT}`);
 });
